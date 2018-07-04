4104121a31794ddc35422c1712fc94cd5cde210f
Olga Magalhaes <olgamagalhaes18@hotmail.com>
2018-07-04 22:42:43 +0100

    test

---
+++ src/Classes/Vetor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import game.classes.Vector;
+
+/**
+ *
+ * @author Olga
+ */
+public class Vetor extends Vector {
+
+    public Vetor() {
+    }
+
+    public Vetor(double scalar1, double scalar2, byte coordianteSystem) {
+        super(scalar1, scalar2, coordianteSystem);
+    }
+    
+    
+}
